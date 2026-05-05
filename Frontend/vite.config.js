8f2d8d9d53f2478e8a9a415194d14fbf5f63967b
Kmalaka-oss <lkodithuwakku@corporate.mediadefined.com>
2026-05-05 13:53:25 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,17 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        host: true, // Exposes the server to the local network
-        port: 3000,
-        proxy: {
-            '/api': {
-                target: 'http://localhost:8000',
-                changeOrigin: true,
-            },
-        },
-    },
+  plugins: [react()],
+
+  server: {
+    host: true,
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true
+      }
+    }
+  },
+
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
 })
